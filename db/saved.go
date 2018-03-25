64d727a956a026057ba907afc0680943acb26c05
my0sot1s <manhte231@gmail.com>
2018-03-26 01:07:46 +0700

    add func for saved

---
+++ db/saved.go
@@ -0,0 +1,5 @@
+package db
+
+func (db *DB) CreateSaved(pid, uid string)             {}
+func (db *DB) RemoveSaved(sid string)                  {}
+func (db *DB) ListSaved(limit int, anchor, uid string) {}
